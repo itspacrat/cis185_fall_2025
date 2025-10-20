87fb10f7118f4e3bb1465a2eb048d2f8b359b5c4
itspacrat <blakemichaelgaynor@gmail.com>
2025-10-20 15:23:18 -0700

    push soln for problem3

--- week4/assignment4/problem3.js
+++ week4/assignment4/problem3.js
@@ -21,9 +21,18 @@ function calculateAverage(scores) {
 }
 
 function dropLowestScore(scores) {
-    // Return a new array with the lowest score removed
-    // If multiple lowest scores exist, remove only one
-    // Don't modify the original array!
+
+    // to sort without modification, clone scores
+    // into another local array using the "spread"
+    // operator during assignment
+    let scoresCopy = [...scores]; // unique object
+
+    /* .sort() is intended for character sorting
+    originally, so we have to throw it an arrow
+    fn as a comparitor, so that it behaves
+    correctly on an array of numbers */
+    return scoresCopy.sort((a, b) => a - b).slice(1, scoresCopy.length);
+
 }
 
 function getLetterGrade(score) {
@@ -34,6 +43,33 @@ function curveGrades(scores, curveAmount) {
     // Add curveAmount to each score
     // Cap all scores at 100 (no score above 100)
     // Return new array, don't modify original
+
+    let curvedGrades = []; // initialize empty
+
+    for (i = 0; i < scores.length; i++) {
+
+        if (scores[i] + curveAmount > 100.) {
+            // clamp to 100
+            curvedGrades.push(100.);
+        } else {
+            // add curved grade to out array
+            curvedGrades.push(scores[i] + curveAmount);
+        }
+
+    }
+
+    return curvedGrades;
+
 }
 
-console.log(calculateAverage([100, 50, 65])); // debug, works
+// use this list as our inout 
+let listOfScores = [80., 50., 33., 21., 73., 100., 23., 100., 98., 95., 30.]; // make sure all behave like floats
+
+// console.log(calculateAverage(listOfScores)); // debug, works
+
+// console.log(dropLowestScore(listOfScores)); // debug, works
+
+// console.log(curveGrades(listOfScores, 3)); // debug, works
+
+// check that listOfScores is not modified
+// console.log(listOfScores); // debug
