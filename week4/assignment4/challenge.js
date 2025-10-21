458f3cd77b3168b1d8acc0105c9948605ff1a793
itspacrat <blakemichaelgaynor@gmail.com>
2025-10-21 11:04:54 -0700

    scaffold challenge 1

--- week4/assignment4/challenge.js
+++ week4/assignment4/challenge.js
@@ -1,6 +1,12 @@
 function decimalToBinary(decimal) {
-    // Convert decimal number to binary string
-    // Don't use toString(2)!
+    /* uses recursion & Math.floor() to simulate "integer" division */
+    if (Math.floor(decimal / 2) == 0) {
+        // clamp to 0 to prevent infinite recursion
+        return decimal % 2; // the final zero
+    } else {
+        return parseInt(/* resulting partial decimal string */ , 10)
+    }
+
 }
 
 function binaryToDecimal(binary) {
@@ -12,4 +18,7 @@ function decimalToHexadecimal(decimal) {
     // Convert decimal to hexadecimal string
     // Use 0-9 and A-F for digits
     // Don't use toString(16)!
+    //decimal = decimal.floor(); // make sure its all int
 }
+
+console.log(decimalToBinary(1.));
