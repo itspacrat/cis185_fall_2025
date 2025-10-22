aad0aa624a60d30d0322b591b0bea5573b8a91d9
itspacrat <blakemichaelgaynor@gmail.com>
2025-10-21 17:43:33 -0700

    attempt challenge

--- week4/assignment4/challenge.js
+++ week4/assignment4/challenge.js
@@ -1,24 +1,13 @@
 function decimalToBinary(decimal) {
-    /* uses recursion & Math.floor() to simulate "integer" division */
-    if (Math.floor(decimal / 2) == 0) {
-        // clamp to 0 to prevent infinite recursion
-        return decimal % 2; // the final zero
-    } else {
-        return parseInt(/* resulting partial decimal string */ , 10)
+    let out = ""
+
+    while (Math.floor(decimal / 2) != 0) {
+        out = out + (decimal % 2);
+        decimal = Math.floor(decimal / 2);
     }
-
+    return out;
 }
 
-function binaryToDecimal(binary) {
-    // Convert binary string to decimal number
-    // Don't use parseInt(binary, 2)!
-}
 
-function decimalToHexadecimal(decimal) {
-    // Convert decimal to hexadecimal string
-    // Use 0-9 and A-F for digits
-    // Don't use toString(16)!
-    //decimal = decimal.floor(); // make sure its all int
-}
-
-console.log(decimalToBinary(1.));
+// console.log(decimalToBinary(1.));
+// BROKEN, did not finish in time :((
