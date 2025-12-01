f6811d173602617907668e56d9dc2e7fba777863
itspacrat <blakemichaelgaynor@gmail.com>
2025-11-30 23:29:36 -0800

    test multiple usestates in NewTask

--- week10/assignment5/src/App.jsx
+++ week10/assignment5/src/App.jsx
@@ -5,25 +5,37 @@ import './App.css'
 
 
 
-function NewTask({ taskName }) {
+function NewTask({ taskName, id }) {
+  // define state tracking for this component
+  // "every NewTask is in charge of remembering its own:"
+  const [title, setTitle] = useState(taskName)
+  const [taskID, setTaskID] = useState(id)
 
+  const handleRenameTask = () => {
+
+  }
   return (
     <>
-      <p>
-        this is a new task, titled: {taskName}
-      </p>
+      <div class="card">
+        <span><input type="text" placeholder="rename task..."></input></span>
+        <button onclick={handleRenameTask}>Rename</button>
+        <p>
+          this is a new task, titled: {taskName}
+        </p>
+      </div>
+
     </>
   )
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
+      <button onClick={() => setCount((count) => count + 1)}>Count: {count}</button>
 
-      <button onclick="{NewTask}">Add Task...</button>
-      <NewTask taskName={"TaskTitle"} />
+
       <NewTask taskName={"TaskTitle"} />
     </>
   )
